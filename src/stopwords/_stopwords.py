5b0cc23f44f3d91bd1c26b6c5dec2fa3b72a25f8
Nisarg Jhaveri <nisargjhaveri@gmail.com>
2016-04-12 23:52:23 +0530

    Update in stopwords, add hindi stopword list

--- src/stopwords/_stopwords.py
+++ src/stopwords/_stopwords.py
@@ -9,10 +9,24 @@ class stopwords:
     def __init__(self, lang):
         self.lang = lang
 
+        self._stopwords = None
+        self._load_list_file()
+
     def _get_list_file(self, mode='r'):
         filename = '%s_stopwords.pickle' % self.lang
         filepath = os.path.join(os.path.dirname(__file__), 'data', filename)
+
+        if not os.path.isfile(filepath):
+            return None
+
         return open(filepath, mode)
+
+    def _load_list_file(self):
+        list_file = self._get_list_file('rb')
+        if not list_file:
+            return
+
+        self._stopwords = cPickle.load(list_file)
 
     def train(self, words, top_k=50):
         word_list = {}
@@ -30,22 +44,28 @@ class stopwords:
         stopwords = map(lambda x: x[0], words_sorted[:top_k])
 
         list_file = self._get_list_file('wb')
-        cPickle.dump(stopwords, list_file)
+        cPickle.dump(stopwords, list_file, 2)
+        self._load_list_file()
 
     def inspect(self):
-        list_file = self._get_list_file('rb')
-        stopwords = cPickle.load(list_file)
-        for word in stopwords:
+        if not self._stopwords:
+            raise Exception(
+                'Stopword list does not exist, you need to train first'
+            )
+
+        for word in self._stopwords:
             print word.encode('utf-8')
 
     def remove(self, sentences):
-        list_file = self._get_list_file('rb')
-        stopwords = cPickle.load(list_file)
+        if not self._stopwords:
+            raise Exception(
+                'Stopword list does not exist, you need to train first'
+            )
 
         clean_sentences = []
         for sent in sentences:
             clean_sentences.append(
-                [token for token in sent if token not in stopwords]
+                [token for token in sent if token not in self._stopwords]
             )
 
         return clean_sentences
